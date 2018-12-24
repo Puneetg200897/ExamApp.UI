e9c160952731b67fd91e5c28ded665ebe2ad47d2
Pankaj <pankajkumarhbti@gmail.com>
2018-12-24 15:14:42 +0530

    Push in MyExamAppBranch

--- ExamApp.Business/Class1.cs
+++ ExamApp.Business/Class1.cs
@@ -10,7 +10,7 @@ namespace ExamApp.Business
     public class Class1
     {
         public string result = string.Empty;
-        dynamic order = new Order().Customer;
+        dynamic order = new Order().Customer.ToString();
         public void Func(Order order)
         {
             var xys = order?.Customer?.Name
